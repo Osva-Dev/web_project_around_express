8933023a087be73b9c02a34819c4620fa9781a9f
Osva-Dev <osvadev30@gmail.com>
2026-03-03 22:31:30 -0600

    chore: initial project structure

--- index.js
+++ index.js
@@ -1,10 +1,7 @@
 const express = require("express");
-// detecta el puerto 3000
 const { PORT = 3000 } = process.env;
-
 const app = express();
 
 app.listen(PORT, () => {
-  // si todo funciona bien, la consola mostrará qué puerto está detectando la aplicación
-  console.log(`App listening at port ${PORT}`);
+  console.log("Ya funciono putos");
 });
