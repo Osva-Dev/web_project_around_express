69acbeb1a6e647dd37e50b360eb8e2d5d5f17a32
Osva-Dev <osvadev30@gmail.com>
2026-03-04 21:34:40 -0600

    add get user by id

--- index.js
+++ index.js
@@ -14,6 +14,16 @@ app.get("/cards", (req, res) => {
   res.json(cards);
 });
 
+app.get("/users/:id", (req, res) => {
+  const user = users.find((u) => u._id === req.params.id);
+
+  if (!user) {
+    return res.status(404).json({ message: "ID de usuario no encontrado" });
+  }
+
+  res.json(user);
+});
+
 app.listen(PORT, () => {
   console.log(`Servidor corriendo en puerto ${PORT}`);
 });
