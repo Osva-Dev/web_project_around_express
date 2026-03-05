f6f5ea634dc332b0abcf3dd9327e4a854849e9af
Osva-Dev <osvadev30@gmail.com>
2026-03-04 21:11:34 -0600

    add get users and get cards

--- index.js
+++ index.js
@@ -1,7 +1,19 @@
 const express = require("express");
 const { PORT = 3000 } = process.env;
+
+const users = require("./data/users.json");
+const cards = require("./data/cards.json");
+
 const app = express();
 
+app.get("/users", (req, res) => {
+  res.json(users);
+});
+
+app.get("/cards", (req, res) => {
+  res.json(cards);
+});
+
 app.listen(PORT, () => {
-  console.log("Ya funciono putos");
+  console.log(`Servidor corriendo en puerto ${PORT}`);
 });
