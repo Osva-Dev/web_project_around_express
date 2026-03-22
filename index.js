d72f4501a7043aa1d89d92db7fd57ef4a7bf7b24
Osva-Dev <osvadev30@gmail.com>
2026-03-21 19:48:58 -0600

    add aroundb conection

--- index.js
+++ index.js
@@ -1,21 +1,39 @@
-const express = require('express');
+require("dotenv").config();
+
+const express = require("express");
+const mongoose = require("mongoose");
 
 const { PORT = 3000 } = process.env;
 
-const usersRouter = require('./routes/users');
-const cardsRouter = require('./routes/cards');
+const usersRouter = require("./routes/users");
+const cardsRouter = require("./routes/cards");
 
 const app = express();
 
 app.use(express.json());
 
-app.use('/users', usersRouter);
-app.use('/cards', cardsRouter);
+app.get("/", (req, res) => {
+  res.send("API funcionando");
+});
+
+app.use("/users", usersRouter);
+app.use("/cards", cardsRouter);
 
 app.use((req, res) => {
-  res.status(404).json({ message: 'Recurso solicitado no encontrado' });
+  res.status(404).json({ message: "Recurso solicitado no encontrado" });
 });
 
-app.listen(PORT, () => {
-  console.log(`Servidor corriendo en puerto ${PORT}`);
-});
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log("MongoDB conectado");
+
+    app.listen(PORT, () => {
+      console.log(`Servidor corriendo en puerto ${PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.error("Error de conexión:", err);
+  });
+
+module.exports = app;
